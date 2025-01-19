d60c55853aceeec530290b58453c9a110017491a
yogie setiawan <setiawanyogie372@gmail.com>
2025-01-19 08:33:56 +0700

    Callback base url & menambahkan proceedd to checkout section

--- cypress/e2e/magentowebtesting.cy.js
+++ cypress/e2e/magentowebtesting.cy.js
@@ -1,5 +1,12 @@
 describe('template spec', () => {
   it('passes', () => {
+    // Callback Base url 
     cy.visit('/')
+
+    
+
+    // Proceed to Checkout section [Yogi]
+    cy.visit('/checkout/#shipping')
+
   })
 })
