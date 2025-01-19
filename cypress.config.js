9ae060c9930b2dd257f2bbe77946942e2f318022
yogie setiawan <setiawanyogie372@gmail.com>
2025-01-19 08:21:16 +0700

    Setting config file dan menambahkan file spec testing utama

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportHeight: 720,
+  viewportWidth: 1280,
+  chromeWebSecurity: false,
+  browser: 'chrome', //Browser defaul saat menjalankan e2e
   e2e: {
+    baseUrl: 'https://magento.softwaretestingboard.com', //base url
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
