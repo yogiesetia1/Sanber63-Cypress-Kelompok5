cb6bb5230751b078ee51edaba6223beb5435afa9
yogie setiawan <setiawanyogie372@gmail.com>
2025-01-20 07:34:56 +0700

    menambahkan test step proceed to checkout

--- cypress/e2e/magentowebtesting.cy.js
+++ cypress/e2e/magentowebtesting.cy.js
@@ -5,10 +5,25 @@ describe('template spec', () => {
 
   })
 })
-describe('Proceed To Checkout Section [Yogi]', () => {
+describe('Verify product can proceed to checkout', () => {
   it('passes', () => {
-    // Base url
-    cy.visit('/checkout/#shipping')
+    cy.visit('/')
+    cy.xpath("//img[@alt='Radiant Tee']").click()
+    cy.clickButton('#option-label-size-143-item-166')
+    cy.clickButton('#option-label-color-93-item-50')
+    cy.xpath("//span[.='Add to Cart']").click()
+    cy.get('.showcart').should('contain','1').click()
+    cy.xpath("//div[@class='amount price-container']//span[@class='price']").should('be.visible').get('#top-cart-btn-checkout').click()
+    cy.get('#customer-email').should('be.visible')
+    
+    // Verifikasi aksi setelah klik
+    cy.url().should('include', '/checkout/#shipping')
 
-  })
+    cy.get('#customer-email').should('be.visible')
+   
+    // Muat data dari file fixture
+    cy.fixture('userAddress.json').then((user) => {
+        cy.inputAddress(user.email, user.firtName, user.lastName, user.company, user.streetAddress, user.city, user.state, user.zipCode, user.phoneNumber)
+    })
+})
 })
