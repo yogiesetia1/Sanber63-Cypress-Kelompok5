326ecf989b24dfd75ae8d9224c3d161a216190ba
yogie setiawan <setiawanyogie372@gmail.com>
2025-01-20 17:48:22 +0700

    update custom command and move file to folder checkout

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -59,17 +59,46 @@ Cypress.Commands.add('clickButton', (selector) => {
 })
 
 Cypress.Commands.add('inputAddress', (email, firstName, lastName, company, streetAddress, city, state, zip, country, phoneNum) => {
-  cy.get('#customer-email').type(email)
-  cy.get('input[name="firstname"]').type(firstName)
-  cy.get('input[name="lastname"]').type(lastName)
+  if(email){
+    cy.get('#customer-email').type(email)
+  }
+  if(firstName){
+    cy.get('input[name="firstname"]').type(firstName)
+  }
+  if(lastName){
+    cy.get('input[name="lastname"]').type(lastName)
+  }
   if (company) {
     cy.get('input[name="company"]').type(company);
   }
-  cy.get('input[name="street[0]"]').type(streetAddress)
-  cy.get('input[name="city"]').type(city)
-  cy.get('select[name="region_id"]').select(state)
-  cy.get('input[name="postcode"]').type(zip)
-  cy.get('select[name="country_id"]').select(country)
-  cy.get('input[name="telephone"]').type(phoneNum)
+  if(streetAddress){
+    cy.get('input[name="street[0]"]').type(streetAddress)
+  }
+  if(city){
+    cy.get('input[name="city"]').type(city)
+  }
+  if(state){
+    cy.get('select[name="region_id"]').select(state)
+  }
+  if(zip){
+    cy.get('input[name="postcode"]').type(zip)
+  }
+  if(country){
+    cy.get('select[name="country_id"]').select(country)
+  }
+  if(phoneNum){
+    cy.get('input[name="telephone"]').type(phoneNum)
+  }
+})
+
+Cypress.Commands.add('addProduct', () => {
+  cy.visit('/')
+  cy.xpath("//img[@alt='Radiant Tee']").click()
+  cy.clickButton('#option-label-size-143-item-166')
+  cy.clickButton('#option-label-color-93-item-50')
+  cy.xpath("//span[.='Add to Cart']").click()
+  cy.get('.showcart').should('contain','1').click()
+  cy.xpath("//div[@class='amount price-container']//span[@class='price']").should('be.visible').get('#top-cart-btn-checkout').click()
+  cy.get('#customer-email').should('be.visible')
 })
   
